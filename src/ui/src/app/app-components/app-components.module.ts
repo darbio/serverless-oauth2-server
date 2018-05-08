df3dbb8f79971b98b914a4be1fe952781b8010d6
darbio <james@darb.io>
2018-05-08 15:24:44 +1000

    font awesome icons

--- src/ui/src/app/app-components/app-components.module.ts
+++ src/ui/src/app/app-components/app-components.module.ts
@@ -1,14 +1,20 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 
 import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 
+library.add(faGoogle, faCoffee);
+
 @NgModule({
   declarations: [LoginComponent, RegisterComponent, ForgotPasswordComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
   exports: [LoginComponent, RegisterComponent, ForgotPasswordComponent]
 })
 export class AppComponentsModule {}
