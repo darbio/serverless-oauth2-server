dbed58e005bc71d11268c926ac29f49e9fde80ab
darbio <james@darb.io>
2018-05-08 14:25:39 +1000

    bootstrap4 ngx-bootstrap working

--- src/ui/src/app/app.module.ts
+++ src/ui/src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
+import { AppBootstrapModule } from "./app-bootstrap.module";
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AppBootstrapModule],
   providers: [],
   bootstrap: [AppComponent]
 })
